fb8685ab5043a9813c809a99b36ccd01ae66661d
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-05-05 21:02:25 +0100

    Create App.jsx

---
+++ 03_Markup with JSX/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+export default function TodoList() {
+  return (
+<>
+  <h1> Amazing Ang's Todo List </h1>
+  <img
+  src="https://pbs.twimg.com/profile_images/1463080286665666564/ubE1IWCT_400x400.jpg"
+  alt="Amazing Ang"
+  className="photo"
+  />
+  <ul>
+    <li> Retrieve hacked funds from the Dark Forest </li>
+    <li> Attend PKU Blockchain Fireside chat</li>
+    <li> Send an update to WTF Academy Discord</li>
+  </ul>
+</>
+);
+}
