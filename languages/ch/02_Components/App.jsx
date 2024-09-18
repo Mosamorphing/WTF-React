6d5b7402d4e1521c4cc735273dc857941f8068fd
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-09-18 02:54:54 -0700

    Create App.jsx

---
+++ languages/ch/02_Components/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function Contributors() {
+  return (
+    <img
+      src="https://pbs.twimg.com/profile_images/1463080286665666564/ubE1IWCT_400x400.jpg"
+      alt="Amazing Ang"
+    />
+  );
+}
+
+export default function Catalogue() {
+  return (
+    <section>
+      <h1>WTF Academy Top Contributors</h1>
+      <div className="Contributors">
+        <Contributors/>
+        <Contributors/>
+        <Contributors/>
+        <Contributors/>
+      </div>
+    </section>
+  );
+}
